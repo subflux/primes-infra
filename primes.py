7520785346d35b615384bc16272cc0215eee3251
Kenneth Gillen <k.h.gillen@dundee.ac.uk>
2018-12-05 18:44:26 +0000

    Seems to work but not to end?

--- primes.py
+++ primes.py
@@ -2,7 +2,7 @@
 
 import sys
 import argparse
-import numpy
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="Include verbose output.",
@@ -29,3 +29,42 @@ if verbose:
 # Implementation
 for x in range(number_of_primes):
     print(x)
+
+print "Trying a range."
+a = np.arange(2,number_of_primes)
+print a
+
+# This is addressible as 0-((number_of_primes+2)-1)
+a = np.ones(number_of_primes + 2, dtype=np.int64)
+print a
+
+print "Iterating through the array."
+for x in range(2,(number_of_primes + 2) - 1):
+    print "Integer is: " + str(x)
+    print a[x]
+
+print "Trying an iterator"
+for x in range(2,(number_of_primes + 2) - 1):
+    iteration_increment = x
+    current_int = x
+
+    print "Integer is: " + str(x)
+    print "Increment is: " + str(x)
+
+    # Until we hit the end of our list...
+    while current_int <= (number_of_primes + 2) - 1:
+        # Move to next incremental int
+        print "current_int: " + str(current_int)
+        # current_int is a prime, so next one, and all
+        # following the same incrementor, are not.
+        current_int = current_int + iteration_increment
+        # flag as NOT PRIME
+        # in case we increment outside the end of the list
+        if current_int <= (number_of_primes + 2) - 1:
+            a[current_int] = 0
+            print a[current_int]
+
+print "Iterating through the array."
+for x in range(2,number_of_primes + 2):
+    if a[x] == 1:
+        print x
